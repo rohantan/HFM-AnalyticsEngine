4cceb4e8aa2e0c59e0f21cb139440002636a9a01
mihir <asthana.mihir@gmail.com>
2014-12-02 12:08:12 -0800

    Removed alerts

--- src/main/webapp/JuniperDashboard/js/juniper.js
+++ src/main/webapp/JuniperDashboard/js/juniper.js
@@ -56,10 +56,10 @@ $(document).ready(function () {
 			url: url,
 			async:false,
 			success: function(msg){
-				alert("msg:"+msg);
+				//alert("msg:"+msg);
 				var obj = jQuery.parseJSON( ''+ msg +'' );
 				$('#intNo1').html(obj.topInterfaces[0]);
-				alert(obj.topInterfaces[0]);
+				//alert(obj.topInterfaces[0]);
 				$('#intNo2').html(obj.topInterfaces[1]);
 				$('#intNo3').html(obj.topInterfaces[2]);
 			}
